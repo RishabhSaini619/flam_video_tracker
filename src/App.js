3e8bea99d826957c7721f0bd52313932eb54e5c2
rishabhsaini619 <rishabhsaini619@gmail.com>
2023-05-22 16:23:18 +0530

    video player

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import React, { useState } from 'react';
+import React, {useRef, useState } from "react";
+import ReactPlayer from "react-player";
+
 
 function App() {
+  
   const [videoURL, setVideoURL] = useState('');
+  const [startTimestamp, setStartTimestamp] = useState("");
+  const [stopTimestamp, setStopTimestamp] = useState("");
+  const [timestamps, setTimestamps] = useState([]);
+  const [videoFilePath, setVideoFilePath] = useState(null);
 
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    const videoURL = URL.createObjectURL(file);
-    setVideoURL(videoURL);
+  const playerRef = useRef(null);
+
+  // const handleFileChange = (event) => {
+  //   const file = event.target.files[0];
+  //   const videoURL = URL.createObjectURL(file);
+  //   setVideoURL(videoURL);
+  // };
+
+  const handleVideoUpload = (event) => {
+    setVideoFilePath(URL.createObjectURL(event.target.files[0]));
+    };
+
+  const formatTimestamp = () => {
+    const time  = playerRef.current.getCurrentTime().toPrecision(2) ;
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time - (hours * 3600)) / 60);
+    const seconds = time - (hours * 3600) - (minutes * 60);
+
+    let timeString = hours.toString().padStart(2, '0') + ':' +
+        minutes.toString().padStart(2, '0') + ':' +
+        seconds.toString().padStart(2, '0');
+
+    return timeString ;
+
+
+  };
+
+  const handleStart = () => {
+    setStartTimestamp(formatTimestamp());
+  };
+
+  const handleStop = () => {
+    setStopTimestamp(formatTimestamp());
+    setStartTimestamp("");
+    setTimestamps([
+      ...timestamps,
+      { start: startTimestamp, stop: formatTimestamp() },
+    ]);
   };
 
   return (
     <div>
-      <input type="file" accept="video/*" onChange={handleFileChange} />
-      {videoURL && (
-        <video controls>
-          <source src={videoURL} type="video/mp4" />
-        </video>
-      )}
+      <input type="file" onChange={handleVideoUpload} />
+
+    
+      
+      <ReactPlayer 
+      ref={playerRef} 
+      url={videoFilePath} width="50%" height="50%" 
+      controls={true}/>
+     <div>
+    <button onClick={handleStart}>Start</button>
+    <button disabled={startTimestamp === ""} onClick={handleStop}>
+      Stop
+    </button>
+  </div>
     </div>
+   
+
   );
 }
+
 
 export default App;
