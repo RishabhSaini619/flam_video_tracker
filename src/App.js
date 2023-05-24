ef25666320dfa97605928c52cd83beb7d7637e78
rishabhsaini619 <rishabhsaini619@gmail.com>
2023-05-24 17:48:28 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import ReactPlayer from "react-player";
-import { CSVLink } from "react-csv";
 import { saveAs } from "file-saver";
+import { Table } from "react-bootstrap";
 
 function App() {
   const [startTimestamp, setStartTimestamp] = useState("");
@@ -10,6 +10,8 @@ function App() {
   const [videoFilePath, setVideoFilePath] = useState(null);
   const [csvData, setCSVData] = useState([]);
   const playerRef = useRef(null);
+  const inputCSVRef = useRef(null);
+  const inputVideoRef = useRef(null);
 
   const handleVideoUpload = (event) => {
     setVideoFilePath(URL.createObjectURL(event.target.files[0]));
@@ -28,86 +30,149 @@ function App() {
         .map((row) => ({ start: row[0].trim(), stop: row[1].trim() }));
 
       setCSVData(parsedCSV);
+
       setTimestamps(parsedCSV);
     };
 
     reader.readAsText(file);
   };
 
-  const formatTimestamp = () => {
-    const time = playerRef.current.getCurrentTime().toPrecision(1);
+  const formatTimestamp = (time) => {
     const hours = Math.floor(time / 3600);
     const minutes = Math.floor((time - hours * 3600) / 60);
-    const seconds = time - hours * 3600 - minutes * 60;
-
+    const seconds = Math.floor(time - hours * 3600 - minutes * 60);
 
     let timeString =
-    hours.toString().padStart(2, "0") +
-    ":" +
-    minutes.toString().padStart(2, "0") +
-    ":" +
-    seconds.toString().padStart(2, "0") ;
+      hours.toString().padStart(2, "0") +
+      ":" +
+      minutes.toString().padStart(2, "0") +
+      ":" +
+      seconds.toString().padStart(2, "0");
 
     return timeString;
   };
 
   const handleStart = () => {
-    setStartTimestamp(formatTimestamp());
+    const currentTime = playerRef.current.getCurrentTime();
+    const formattedTimestamp = formatTimestamp(currentTime);
+    setStartTimestamp(formattedTimestamp);
   };
 
   const handleStop = () => {
-    setStopTimestamp(formatTimestamp());
-    setStartTimestamp("");
+    const currentTime = playerRef.current.getCurrentTime();
+    const formattedTimestamp = formatTimestamp(currentTime);
+
+    setStopTimestamp(formattedTimestamp);
     setTimestamps([
       ...timestamps,
-      { start: startTimestamp, stop: formatTimestamp() },
+      { start: startTimestamp, stop: formattedTimestamp },
     ]);
+    setStartTimestamp("");
   };
 
   const handleDownload = () => {
-    const csvData = [["Start Time", "Stop Time"]];
+    const csvRows = [null];
     timestamps.forEach((timestamp) => {
-      csvData.push([timestamp.start, timestamp.stop]);
+      csvRows?.push([timestamp.start, timestamp.stop]);
     });
-    const blob = new Blob([csvData.join("\n")], {
+    const csvData = csvRows?.map((row) => row.join(",")).join("\n");
+    const blob = new Blob([csvData], {
       type: "text/csv;charset=utf-8",
     });
     saveAs(blob, "timestamps.csv");
   };
 
+  const [showHead, setShowHead] = useState(false);
+
   return (
-    <div>
+    <>
+      {/* Nav Bar */}
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             Flam Video Tracker
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">
-                  Home
+                  <button
+                    className="btn btn-outline-success"
+                    type="file"
+                    onClick={() => inputVideoRef.current.click()}
+                  >
+                    Pick Video
+                  </button>
+                  <input
+                    type="file"
+                    style={{ display: "none" }}
+                    onChange={handleVideoUpload}
+                    ref={inputVideoRef}
+                  />
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  About
+                <a className="nav-link active" aria-current="page" href="#">
+                  <button
+                    className="btn btn-outline-success"
+                    type="button"
+                    onClick={() => inputCSVRef.current.click()}
+                  >
+                    Pick CSV
+                  </button>
+                  <input
+                    type="file"
+                    style={{ display: "none" }}
+                    onChange={handleCSVUpload}
+                    ref={inputCSVRef}
+                  />
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Contact
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="#"
+                  onClick={handleStart}
+                >
+                  <button
+                    className="btn btn-outline-success"
+                    type="button"
+                    onClick={handleStart}
+                  >
+                    Start
+                  </button>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="#"
+                  onClick={handleStop}
+                >
+                  <button
+                    className="btn btn-outline-success"
+                    type="button"
+                    disabled={startTimestamp === ""}
+                    onClick={handleStop}
+                  >
+                    Stop
+                  </button>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  {csvData.length > 0 || timestamps.length > 0 ? (
+                    <button
+                      className="btn btn-outline-success"
+                      type="button"
+                      disabled={!timestamps.length}
+                      onClick={handleDownload}
+                    >
+                      Download
+                    </button>
+                  ) : null}
                 </a>
               </li>
             </ul>
@@ -115,68 +180,57 @@ function App() {
         </div>
       </nav>
 
-      <div className="button-container">
-        <input type="file" onChange={handleVideoUpload} />
-
-        <button onClick={handleStart}>Start</button>
-        <button disabled={startTimestamp === ""} onClick={handleStop}>
-          Stop{" "}
-        </button>
-
-        <input type="file" onChange={handleCSVUpload} />
-        {csvData.length > 0 && (
-          <button disabled={!csvData.length} onClick={handleDownload}>
-            Download CSV
-          </button>
-        )}
+      {/* Player */}
+      <div className="video-player-container">
+        <ReactPlayer
+          ref={playerRef}
+          url={videoFilePath}
+          width="70%"
+          height="10%"
+          controls={true}
+        />
       </div>
+      {/* csv Table */}
+      <div className="card-table">
+        <Table >
+          {timestamps.length >= 1 && (
+            <thead
+              style={{
+                backgroundColor: "lightgrey",
+                color: "blue",
+                borderStyle: "soild",
+                borderColor: "black",
+              }}
+            >
+              {" "}
+              <tr>
+                <th>S No.</th>
+                <th>Start Time</th>
+                <th>Stop Time</th>
+              </tr>
+            </thead>
+          )}
 
-      <div class="float-container">
-        <div class="float-child">
-            <div className="video-player-container">
-              <ReactPlayer
-                ref={playerRef}
-                url={videoFilePath}
-                width="50%"
-                height="50%"
-                controls={true}
-              />
-            </div>
-        </div>
-
-        <div class="float-child">
-            <div className="table-container">
-              <h5>Table:</h5>
-              <table>
-                <thead
-                  style={{
-                    backgroundColor: "lightgray",
-                    color: "blue",
-                    borderStyle: "groove",
-                    borderColor: "black",
-                  }}
-                >
-                  {" "}
-                  <tr>
-                    <th> S No.</th>
-                    <th>Start</th>
-                    <th>Stop</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {timestamps.map((timestamp, index) => (
-                    <tr key={index}>
-                      <td>{index + 1}</td>
-                      <td>{timestamp.start}</td>
-                      <td>{timestamp.stop}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-        </div>
+          <tbody
+            className="table-group-divider"
+            style={{
+              backgroundColor: "white",
+              color: "black",
+              borderStyle: "solid",
+              borderColor: "black",
+            }}
+          >
+            {timestamps.map((timestamp, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{timestamp.start}</td>
+                <td>{timestamp.stop}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
-    </div>
+    </>
   );
 }
 
