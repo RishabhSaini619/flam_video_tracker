5d00c84cc222bad50f68e2126f624d79b79b3a25
rishabhsaini619 <rishabhsaini619@gmail.com>
2023-05-24 18:26:00 +0530

    video-player

---
+++ src/components/VideoPlayer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactPlayer from "react-player";
+
+function VideoPlayer({ playerRef, videoFilePath, handleStart, startTimestamp }) {
+  return (
+    <div className="video-player-container">
+      <ReactPlayer
+        ref={playerRef}
+        url={videoFilePath}
+        width="70%"
+        height="10%"
+        controls={true}
+      />
+    </div>
+  );
+}
+
+export default VideoPlayer;
